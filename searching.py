1946aaf115a22af0bbc5f8744757c9aaa6cbe7c1
Josefína Carasová <jcarasova@gmail.com>
2022-04-11 08:53:03 +0200

    prvni commit

--- searching.py
+++ searching.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 # get current working directory path
 cwd_path = os.getcwd()
@@ -11,11 +12,20 @@ def read_data(file_name, field):
     :param field: (str), field of a dict to return
     :return: (list, string),
     """
+
+
     file_path = os.path.join(cwd_path, file_name)
+    with open(file_path, mode="r") as json_file:
+        data = json.load(json_file)
+    #print(set(data.keys())) # ziskame mnozinu klicu
+    if field not in set(data.keys()):
+        return None
+    return data[field] #field nam vytiskne jen to pole, ktere odpovida klici
 
 
 def main():
-    pass
+    sequential_data = read_data("sequential.json", "unordered_numbers")
+    print(sequential_data)
 
 
 if __name__ == '__main__':
